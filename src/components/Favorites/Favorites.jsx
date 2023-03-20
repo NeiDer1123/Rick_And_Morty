05bf7de9f99bc6c3f10f7b464dad6f0730c38525
NeiDer1123 <neybm1123@gmail.com>
2023-03-19 21:11:12 -0500

    Styles

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,15 +1,25 @@
 import { connect } from "react-redux";
+import styles from "../Favorites/Favorite.module.css";
+import { NavLink } from "react-router-dom";
 
 function Favorites({ favorites }) {
   return (
-    <div>
+    <div className={styles.container}>
       {favorites.map((character) => {
         return (
-            <div key={character.id}>
-                <h2>{character.name}</h2>
-                <img src={character.image} alt={character.name} />
-            </div>
-        )
+          <div key={character.id} className={styles.card}>
+            {/* <h2 className={styles.h2}>{character.name}</h2> */}
+            <NavLink to={`/detail/${character.id}`} className={styles.link}>
+              <h2 className={styles.h2}>{character.name}</h2>
+            </NavLink>
+            <h2 className={styles.h2}>{character.species}</h2>
+            <img
+              src={character.image}
+              alt={character.name}
+              className={styles.img}
+            />
+          </div>
+        );
       })}
     </div>
   );
