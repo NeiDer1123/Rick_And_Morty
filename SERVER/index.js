d602a9b497d9b20f22619ae7ded1f3d882f4e434
NeiDer1123 <neybm1123@gmail.com>
2023-04-07 20:44:41 -0500

    Promises

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,22 +1,32 @@
 const http = require("http");
-const data = require("./src/utils/data");
+// const data = require("./src/utils/data");
 const PORT = 3001;
+const getCharById = require("./src/controllers/getCharById");
 
 http
   .createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
 
+    // if (req.url.includes("/rickandmorty/character")) {
+    //   const id = req.url.split("/").pop();
+    //   // const id = req.url.split("/").at(-1); ---> OTRA FORMA
+    //   const char = data.find((char) => char.id == id);
+    //   // const char = data[id-1]; ---> OTRA FORMA
+    //   // const char = data.filter((char)=> char.id == id)[0] ---> OTRA FORMA
+    //   res.writeHead(200, { "Content-Type": "application/json" });
+    //   res.end(JSON.stringify(char));
+
+    //   return;
+    // }
+    // res.end(404)
+
     if (req.url.includes("/rickandmorty/character")) {
       const id = req.url.split("/").pop();
-      // const id = req.url.split("/").at(-1); ---> OTRA FORMA
-      const char = data.find((char) => char.id == id);
-      // const char = data[id-1]; ---> OTRA FORMA
-      // const char = data.filter((char)=> char.id == id)[0] ---> OTRA FORMA
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(char));
+      getCharById(res, id);
 
       return;
     }
-    res.end(404)
+
+    res.end(404);
   })
   .listen(PORT, "localhost");
